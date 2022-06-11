83c3240ff161cb950b9ca0467aaf5b3aaedfacc1
suman <sumankhanal770@gmail.com>
2022-06-11 17:18:04 +0545

    cleaned up

--- tic-tac-toe.py
+++ tic-tac-toe.py
@@ -90,21 +90,28 @@ def checkIfWon(xoro):
 def reset():
     global b1,b2,b3,b4,b5,b6,b7,b8,b9
     global count,clicked
+    count = 0
+    clicked = True
     b1 = Button(root, text='', font=("helvetica", 20), height=3, width=6, bg="systemButtonFace",command=lambda: b_click(b1))
     b2 = Button(root, text='', font=("helvetica", 20), height=3, width=6, bg="systemButtonFace",command=lambda: b_click(b2))
     b3 = Button(root, text='', font=("helvetica", 20), height=3, width=6, bg="systemButtonFace",command=lambda: b_click(b3))
+
     b4 = Button(root, text='', font=("helvetica", 20), height=3, width=6, bg="systemButtonFace",command=lambda: b_click(b4))
     b5 = Button(root, text='', font=("helvetica", 20), height=3, width=6, bg="systemButtonFace",command=lambda: b_click(b5))
     b6 = Button(root, text='', font=("helvetica", 20), height=3, width=6, bg="systemButtonFace",command=lambda: b_click(b6))
+
     b7 = Button(root, text='', font=("helvetica", 20), height=3, width=6, bg="systemButtonFace",command=lambda: b_click(b7))
     b8 = Button(root, text='', font=("helvetica", 20), height=3, width=6, bg="systemButtonFace",command=lambda: b_click(b8))
     b9 = Button(root, text='', font=("helvetica", 20), height=3, width=6, bg="systemButtonFace",command=lambda: b_click(b9))
+
     b1.grid(row=0, column=0)
     b2.grid(row=0, column=1)
     b3.grid(row=0, column=2)
+
     b4.grid(row=1, column=0)
     b5.grid(row=1, column=1)
     b6.grid(row=1, column=2)
+
     b7.grid(row=2, column=0)
     b8.grid(row=2, column=1)
     b9.grid(row=2, column=2)
@@ -126,31 +133,7 @@ def b_click(b):
     else:
         messagebox.showerror("tic tac toe","click somewhere else")
 
-
-b1 = Button(root, text = '', font = ("helvetica", 20), height= 3, width=6, bg = "systemButtonFace", command= lambda:b_click(b1))
-b2 = Button(root, text = '', font = ("helvetica", 20), height= 3, width=6, bg = "systemButtonFace", command= lambda:b_click(b2))
-b3 = Button(root, text = '', font = ("helvetica", 20), height= 3, width=6, bg = "systemButtonFace", command= lambda:b_click(b3))
-
-b4 = Button(root, text = '', font = ("helvetica", 20), height= 3, width=6, bg = "systemButtonFace", command= lambda:b_click(b4))
-b5 = Button(root, text = '', font = ("helvetica", 20), height= 3, width=6, bg = "systemButtonFace", command= lambda:b_click(b5))
-b6 = Button(root, text = '', font = ("helvetica", 20), height= 3, width=6, bg = "systemButtonFace", command= lambda:b_click(b6))
-
-b7 = Button(root, text = '', font = ("helvetica", 20), height= 3, width=6, bg = "systemButtonFace", command= lambda:b_click(b7))
-b8 = Button(root, text = '', font = ("helvetica", 20), height= 3, width=6, bg = "systemButtonFace", command= lambda:b_click(b8))
-b9 = Button(root, text = '', font = ("helvetica", 20), height= 3, width=6, bg = "systemButtonFace", command= lambda:b_click(b9))
-
-b1.grid(row=0, column=0)
-b2.grid(row=0, column=1)
-b3.grid(row=0, column=2)
-
-b4.grid(row=1, column=0)
-b5.grid(row=1, column=1)
-b6.grid(row=1, column=2)
-
-b7.grid(row=2, column=0)
-b8.grid(row=2, column=1)
-b9.grid(row=2, column=2)
-
+reset()
 #create menu
 my_menu = Menu(root)
 root.config(menu=my_menu)
